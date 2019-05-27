c52bf1c2be42784c78ff298f15b9d3ee83b2117e
Andrius <andrius.gecys@gmail.com>
2019-05-27 23:18:13 +0200

    Finished and polished assign employee window. Project list in time registration window is now sorted by customer name, not customer ID as before. Closed projects removed from the project list.

--- co_manager/views.py
+++ co_manager/views.py
@@ -5,6 +5,6 @@ from receivables.models import Project, SalesOrderHeader, SalesOrderLine
 
 @login_required(login_url='/accounts/login/')
 def index(request):
-    projects = Project.objects.filter(employees__in = [request.user.employee]).order_by('customer', 'name')
+    projects = Project.objects.filter(employees__in = [request.user.employee], active = True).order_by('customer__name', 'name').select_related('customer')
     return render(request, 'salary/registration_list.html', {'projects': projects})
     #return render_to_response('co_manager/index.html')
